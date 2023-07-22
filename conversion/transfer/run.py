106b83b8c3762a3860105e0748d853e4ae4b8678
ermayankrajput <er.mayankrajput95@gmail.com>
2023-07-22 20:09:20 +0530

    Update run.py

--- conversion/transfer/run.py
+++ conversion/transfer/run.py
@@ -36,6 +36,6 @@ from  os.path import abspath, dirname
 from transfer import main
 
 aSource = abspath(dirname(Path(__file__).resolve()) + r"/../../models/omni_wheel.stp")
-aDest = abspath(dirname(Path(__file__).resolve()) + r"/omni_wheel.jpg")
+aDest = abspath(dirname(Path(__file__).resolve()) + r"/omni_wheel.pdf")
 
 sys.exit(main(aSource, aDest))
