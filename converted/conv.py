3592a823869b87b9c6278ef9d7f74cbfb9fef13a
ermayankrajput <er.mayankrajput95@gmail.com>
2023-07-25 09:17:31 +0530

    Update conv.py

--- converted/conv.py
+++ converted/conv.py
@@ -43,7 +43,7 @@ def conversion():
     s3UploadedFile = s3_upload(fileServerPath, newFileName)
     s3ImageFile = s3_upload('uploads/' + newFileName + '.png', newFileName + '.png')
     s3TransportedFile = s3_upload('uploads/' + newFileName + '.stl', newFileName + '.stl')
-    dimensions = stlToImg('uploads/' + newFileName + '.stl' 'uploads/temp.png')
+    dimensions = stlToImg('uploads/' + newFileName + '.stl', 'uploads/temp.png')
     for f in os.listdir('uploads'):
         os.remove(os.path.join('uploads', f))
     return jsonify({"success": True,"uploded_file": s3UploadedFile, "transported_file": newFileName + '.stl',"image_file": newFileName + '.png', "x":str(dimensions.get("x")),"y":str(dimensions.get("y")),"z":str(dimensions.get("z"))})
