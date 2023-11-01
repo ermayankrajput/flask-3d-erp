586cbf65a38d90782f0df0a85e7837cbb2ff4e88
prashant sirohi <er.mayankrajput95@gmail.com>
2023-11-01 12:13:17 +0530

    tes

--- migrations/versions/983b75d61c9a_create_roles_.py
+++ migrations/versions/983b75d61c9a_create_roles_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '983b75d61c9a_create_roles_'
-down_revision = '983b75d61c9a_create_roles'
+down_revision = '983b75d61c9a'
 branch_labels = None
 depends_on = None
 
