56c8343ae1dfc8a692d8fa5f43dac3f846d1afad
Arun Pratap <arun@Aruns-MacBook-Air.local>
2024-06-11 18:25:07 +0530

    check if file already exist while downloading zip

--- s3_upload.py
+++ s3_upload.py
@@ -1,4 +1,5 @@
 import boto3
+import os.path
 
 def s3_upload(filePath, newFileName):
     s3 = boto3.resource("s3", aws_access_key_id="AKIAVJYKM3CKTXR2RUTW", aws_secret_access_key= "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")
@@ -13,6 +14,7 @@ def downloadFile(filePath,path):
     # s3.Bucket.download_file('elasticbeanstalk-ap-northeast-1-364557162645', filePath, 'hello2.txt')
     s3 = boto3.client("s3", aws_access_key_id="AKIAVJYKM3CKTXR2RUTW", aws_secret_access_key= "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")
     # breakpoint()
-    s3.download_file('elasticbeanstalk-ap-northeast-1-364557162645', filePath, path+'/'+filePath.split("/")[-1])
+    if not os.path.isfile(path+'/'+filePath.split("/")[-1]):
+        s3.download_file('elasticbeanstalk-ap-northeast-1-364557162645', filePath, path+'/'+filePath.split("/")[-1])
     # s3.download_file('elasticbeanstalk-ap-northeast-1-364557162645', filePath, filePath)
     # s3.Bucket(bucketName).download_file(filePath)
