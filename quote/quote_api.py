03d2a0f950d16257addd9f103eec6355cb924dda
Mayank Rajput <er.mayankrajput95@gmail.com>
2025-10-26 19:53:28 +0530

    wedfev

--- quote/quote_api.py
+++ quote/quote_api.py
@@ -652,7 +652,8 @@ def handle3dFiles(file, filename, quote):
         p.join()
         queueInfo2 = queue.get() if not queue.empty() else {"x": 0, "y": 0, "z": 0}
         # unpack tuple safely
-        print('queueInfo2: ', queueInfo2, 'queue.get()', queue.get(), queueInfo2.x)
+        print('queueInfo2: ', queueInfo2, 'queue.get()', queue.get())
+        print('hbvh', queueInfo2.get("x"), queueInfo2['x']);
         # breakpoint()
         file_data_list = {
             "file_name": filename,
